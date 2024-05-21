f3ac0e5ced30159fe69b1aae55b96e07f1a7116d
soulrafael <rafaelbsoul@gmail.com>
2024-05-21 09:01:09 -0300

    implementação teste

--- ControleDeFluxo/Controle-Fluxo/src/CaixaEletronico.java
+++ ControleDeFluxo/Controle-Fluxo/src/CaixaEletronico.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+import ContaBanco.ContaBanco;
+import ContaBanco.ContaBanco;
+//Controle de Fluxo Simples até as 8:50
 public class CaixaEletronico{
 public static void main(String[] args) {
-    double saldo =  25.0;
-    double valorSolicitado = 17.0;
+    Scanner entrada = new Scanner(System.in);// Entrada de dados via terminal
+    ContaBanco usuarioAtual = new ContaBanco();
+    usuarioAtual.Usuario = entrada.nextLine();
+    usuarioAtual.agencia = entrada.next();
+    usuarioAtual.numeroConta = entrada.nextInt();
+
+System.out.println("Teste :" + usuarioAtual.Usuario + usuarioAtual.agencia+ usuarioAtual.numeroConta);
+
+
+
+
+
+
+
+    /*double saldo =  25.0;
+    double valorSolicitado=17.0;
     if (valorSolicitado<saldo) {
         saldo= saldo-valorSolicitado;
         System.out.println("saldo atual "+ saldo);
-    }
+    }*/
 }
 
 
---
+++ ControleDeFluxo/Controle-Fluxo/src/ContaBanco/ContaBanco.java
@@ -0,0 +1,8 @@
+package ContaBanco;
+public class ContaBanco {
+    public String Usuario;
+    public double saldoUsuario;
+    public int numeroConta;
+    public String agencia;
+    
+}
