3e3c91f2d5df85cf7b5e67c189f7e2aa73fe41c2
soulrafael <rafaelbsoul@gmail.com>
2024-06-12 21:16:23 -0300

    Collections Java

---
+++ colections-java-api-2024/collections-java-api-2024/src/Colecao.java
@@ -0,0 +1,11 @@
+import list.ListaTarefas;
+
+public class Colecao {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        ListaTarefas Tarefas = new ListaTarefas();
+        Tarefas.adicionarTarefa( "Ar");
+    }
+
+
+}
---
+++ colections-java-api-2024/collections-java-api-2024/src/list/ListaTarefas.java
@@ -0,0 +1,17 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    private List<Tarefas> tarefalista;
+
+
+    public void adicionarTarefa(String descricao){
+            List ListaTarefas = new ArrayList<>();
+            ListaTarefas.add("test Gustavo");
+            System.out.println(ListaTarefas);
+        
+    }
+
+}
