103f425a34161ac48e33db182eb09a63ae97af9c
soulrafael <rafaelbsoul@gmail.com>
2024-06-13 22:51:12 -0300

    test1

---
+++ colections-java-api-2024/collections-java-api-2024/src/list/Coisas.java
@@ -0,0 +1,12 @@
+package list;
+
+public class Coisas {
+
+    private String coisa;
+
+    public Coisas(String coista) {
+        //TODO Auto-generated constructor stub
+        this.coisa = coista;
+    }
+
+}
